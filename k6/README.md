fa63c1ec9c4891f566fa90e5d606c891ce32464f
Michał Kaźmierczak <michalkazmierczak@icloud.com>
2024-01-21 22:18:41 +0100

    add k6 tests for synthetic traffic

---
+++ k6/README.md
@@ -0,0 +1,11 @@
+# k6 synthetic load generator
+
+The purpose of using `k6` is to generate synthetic traffic for presentation purposes.
+
+## Running it
+
+Having the [CLI installed](https://k6.io/docs/get-started/installation/), run the following command:
+
+```
+k6 run --vus 10 --duration 30s script.js
+```
---
+++ k6/script.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+
+export default function () {
+  // in every iteration the odds of an endpoint being called are:
+  //  - 60% for "/demo/database_bulk_read"
+  //  - 30% for "/demo/database_bulk_insert"
+  //  - 10% for "/demo/database_delete_all"
+
+  const chance = Math.random();
+  switch (true) {
+    case (chance < 0.6):
+      var endpoint = '/demo/database_bulk_read';
+      break;
+    case (chance < 0.9):
+      var endpoint = '/demo/database_bulk_insert';
+      break;
+    default:
+      var endpoint = '/demo/database_delete_all';
+  }
+
+  http.get('http://localhost:3000' + endpoint);
+}
