d2adf975869eaa0c1b2f72c07fb95a329647b8a8
Michał Kaźmierczak <michalkazmierczak@icloud.com>
2025-04-29 23:22:44 +0200

    init Kamal deployment

---
+++ rails_app/config/deploy/postgres/setup.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE opentelemetry_rails_example_production;
+CREATE DATABASE opentelemetry_rails_example_production_queue;
+-- CREATE DATABASE race_scraper_production_cable;
+-- CREATE DATABASE race_scraper_production_cache;
