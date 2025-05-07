b6526d8b452eafc4a07feba04675fe426f973649
Michał Kaźmierczak <michalkazmierczak@icloud.com>
2025-05-07 08:24:46 +0200

    fix DB names

--- rails_app/config/deploy/postgres/setup.sql
+++ rails_app/config/deploy/postgres/setup.sql
@@ -1,4 +1,2 @@
-CREATE DATABASE opentelemetry_rails_example_production;
-CREATE DATABASE opentelemetry_rails_example_production_queue;
--- CREATE DATABASE race_scraper_production_cable;
--- CREATE DATABASE race_scraper_production_cache;
+CREATE DATABASE opentelemetry_rails_production;
+CREATE DATABASE opentelemetry_rails_production_queue;
